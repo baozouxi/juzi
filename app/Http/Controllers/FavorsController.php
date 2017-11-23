887633a09dc3385c1b0e5e853cbce7e2a4f88565
Your Name <you@example.com>
2017-11-23 13:49:39 +0800

    zk

--- app/Http/Controllers/FavorsController.php
+++ app/Http/Controllers/FavorsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Passage;
 use Illuminate\Http\Request;
 
 class FavorsController extends Controller
@@ -10,6 +11,12 @@ class FavorsController extends Controller
 
     public function store(Request $request)
     {
+        $passage = Passage::findOrFail($request->passage_id);
+
+        $user_id = session()->get('login_user')->id;
+
+        $passage->favors()->create(['user_id'=>$user_id]);
+
 
     }
 
