e65042041c7ea38b4c5060ccf217b8ade0e6650d
Your Name <you@example.com>
2017-11-23 16:10:56 +0800

    l

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,24 +21,27 @@
 <div id="article_list">
 
     @foreach($passages as $passage)
-    <div class="article_item">
-        <div class="item_wrap">
-            <h1>《{{ $passage->from }}》-- {{ $passage->author }}</h1>
-            <div class="a_span">
-                <span>创业艰难</span>
-            </div>
-            <div class="content">{{ $passage->content }}
-            </div>
+        <div class="article_item">
+            <div class="item_wrap">
+                <h1>《{{ $passage->from }}》-- {{ $passage->author }}</h1>
+                <div class="a_span">
+                    <span>创业艰难</span>
+                </div>
+                <div class="content">{{ $passage->content }}
+                </div>
 
-            <div class="article_btn tr">
+                <div class="article_btn tr">
 
-                <span class=" {{ $passage->liked ? 'liked menu_active' : 'like' }} " data-passage_id="{{ $passage->id }}" ><i class="iconfont">&#xe600;</i>{{ $passage->favors_count }}</span>
-                <a href="{{ route('passages.show', ['id'=>$passage->id]) }}"><span><i class="iconfont">&#xe624;</i>{{ $passage->comments_count }}</span></a>
-                <span class="copy_btn"><i class="iconfont">&#xe6ea;</i>复制</span>
+                    <span class=" {{ $passage->liked ? 'liked menu_active' : 'like' }} "
+                          data-passage_id="{{ $passage->id }}"><i
+                                class="iconfont">&#xe600;</i>{{ $passage->favors_count }}</span>
+                    <a href="{{ route('passages.show', ['id'=>$passage->id]) }}"><span><i
+                                    class="iconfont">&#xe624;</i>{{ $passage->comments_count }}</span></a>
+                    <span class="copy_btn"><i class="iconfont">&#xe6ea;</i>复制</span>
 
+                </div>
             </div>
         </div>
-    </div>
     @endforeach
 </div>
 
@@ -50,7 +53,11 @@
         </span></a>
     <a href="{{ route('passages.create') }}"> <span>
             <p class="iconfont">&#xe601;</p>
-            <p>发布</p>
+            <p>发布句子</p>
+        </span></a>
+    <a href="{{ route('labels.create') }}"> <span>
+            <p class="iconfont">&#xe601;</p>
+            <p>发布标签</p>
         </span></a>
     <a href="{{ route('me') }}"><span>
             <p class="iconfont">&#xe740;</p>
