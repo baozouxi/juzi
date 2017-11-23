51a29f4112e7ca377a425742d0760dbd198e7a9c
Your Name <you@example.com>
2017-11-23 11:12:55 +0800

    zz

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,11 +22,11 @@
     @foreach($passages as $passage)
     <div class="article_item">
         <div class="item_wrap">
-            <h1>《{{ $passage->title }}》-- 吴晓波</h1>
+            <h1>《{{ $passage->from }}》-- {{ $passage->author }}</h1>
             <div class="a_span">
                 <span>创业艰难</span>
             </div>
-            <div class="content">“他之所以糟糕，不是因为技术上不承受，而是它违背了一条非常简单却不易被察觉的竞争原则：在一个缺乏成长性的行业里，任何创新都很难获得等值的回报，因而是没有意义的！
+            <div class="content">{{ $passage->content }}
             </div>
 
             <div class="article_btn tr">
