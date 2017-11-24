cefae416670ec2cd742c2cb232b80f1f6cde185b
Your Name <you@example.com>
2017-11-24 11:51:03 +0800

    j

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -81,10 +81,10 @@
 
     clipboard.on('success', function(e){
         console.log(e);
-        document.getElementById('copyCode').innerHTML = '复制成功';
+
     });
     clipboard.on('error', function(e){
-        document.getElementById('copyCode').innerHTML = '复制失败，请长按复制';
+     
     });
 
 
