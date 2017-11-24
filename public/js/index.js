cad2aa4b5f23280116c56babe127f0a280a58ccc
Your Name <you@example.com>
2017-11-24 13:11:31 +0800

    j

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,11 @@
 $(function(){
 
     var clipboard = new Clipboard('.copy_btn');
+    
     clipboard.on('success', function(e){
         console.log(e);
     });
     clipboard.on('error', function(e){
     });
 
-})
+});
