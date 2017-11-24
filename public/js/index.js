4b0466a5a6a1310d53cd1fd9218d37f0744b0542
Your Name <you@example.com>
2017-11-24 11:48:26 +0800

    ks

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,13 @@ $(function(){
 
 
     var clipboard = new Clipboard('.copy_btn');
-
+    
+    clipboard.on('success', function(e){
+        console.log(e);
+        document.getElementById('copyCode').innerHTML = '复制成功';
+    });
+    clipboard.on('error', function(e){
+        document.getElementById('copyCode').innerHTML = '复制失败，请长按复制';
+    });
 
 })
