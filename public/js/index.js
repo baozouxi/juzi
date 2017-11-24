bb006c08afb042a740be6e112dbbacb117d358a5
Your Name <you@example.com>
2017-11-24 11:49:59 +0800

    j

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,5 @@
 $(function(){
 
 
-    var clipboard = new Clipboard('.copy_btn');
-    
-    clipboard.on('success', function(e){
-        console.log(e);
-        document.getElementById('copyCode').innerHTML = '复制成功';
-    });
-    clipboard.on('error', function(e){
-        document.getElementById('copyCode').innerHTML = '复制失败，请长按复制';
-    });
 
 })
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -76,6 +76,21 @@
 <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
 <script src="{{ asset('js/clipboard.min.js') }}"></script>
 <script src="{{ asset('js/com.js') }}"></script>
+<script>
+    var clipboard = new Clipboard('.copy_btn');
+
+    clipboard.on('success', function(e){
+        console.log(e);
+        document.getElementById('copyCode').innerHTML = '复制成功';
+    });
+    clipboard.on('error', function(e){
+        document.getElementById('copyCode').innerHTML = '复制失败，请长按复制';
+    });
+
+
+
+</script>
+
 <script type="text/javascript" src="{{ asset('js/index.js') }}"></script>
 
 
