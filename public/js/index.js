16dab7e5702e75027c62f4063a8691ca5d3d7319
Your Name <you@example.com>
2017-11-24 13:07:39 +0800

    k

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,10 @@
 $(function(){
 
     var clipboard = new Clipboard('.copy_btn');
-
     clipboard.on('success', function(e){
-        alert('复制成功');
+        console.log(e);
     });
     clipboard.on('error', function(e){
-        alert('复制失败');
     });
-    
+
 })
