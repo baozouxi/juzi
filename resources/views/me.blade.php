e1805805c9cccd244132641a8e092303528be13e
Your Name <you@example.com>
2017-11-25 10:08:28 +0800

    k

--- resources/views/me.blade.php
+++ resources/views/me.blade.php
@@ -19,7 +19,7 @@
         <div class="nick_text fl">
             <div class="nickname">{{ $user->nickname }}</div>
             <div class="fav">
-                <span>发布 {{ $user->passages()->count() }}</span>
+                <span>发布 {{ $user->passages->count() }}</span>
                 <span style="margin-left: 0.1rem;">点赞 {{ $user->favors()->count() }}</span>
             </div>
         </div>
