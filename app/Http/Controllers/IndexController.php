1c0180ceaae8c08c3ca88fca6da346fc690448cb
Your Name <you@example.com>
2017-11-25 10:14:41 +0800

    k

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -56,7 +56,8 @@ class IndexController extends Controller
 
             case 'like':
                 $passages = new Collection();
-                 dd($user->favors()->passage());
+                $favors = $user->favors();
+                dd($favors);
                 foreach ($user->favors as $favor) {
                     if ($favor->passage !== null && $favor->passage->checked == 1 ) {
                         $passages->add($favor->passage);
