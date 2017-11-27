2b36e04d55da565071d0b4c9afe04fc2d1d2b19e
Your Name <you@example.com>
2017-11-27 13:15:55 +0800

    j

--- resources/views/passage/list.blade.php
+++ resources/views/passage/list.blade.php
@@ -85,6 +85,14 @@
             });
 
 
+            table.on('edit(test)', function(obj){
+                var value = obj.value //得到修改后的值
+                    ,data = obj.data //得到所在行所有键值
+                    ,field = obj.field; //得到字段
+                layer.msg('[ID: '+ data.id +'] ' + field + ' 字段更改为：'+ value);
+            });
+
+
         });
     </script>
 @endpush
